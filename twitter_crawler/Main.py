4881b8c9ed9c8b072fff57fee026dd56934ad144
Osman Cihan Kilinc <okilinc@ucsd.edu>
2018-02-28 19:26:28 -0800

    Comments and asserts were added.

--- twitter_crawler/Main.py
+++ twitter_crawler/Main.py
@@ -6,11 +6,10 @@ import csv
 def main():
 
 	tweet_criteria = manager.TweetCriteria()
-	tweet_criteria.setQuerySearch("UnitedAirlines").setMaxTweets(100)
+	tweet_criteria.setQuerySearch("UnitedAirlines").setMaxTweets(5)
 	tweets = manager.TweetManager().getTweets(tweet_criteria)
 	print str(tweet_criteria)
 	for i in range(len(tweets)):
-	    #tweets[i] = TweetHelper.parseTweetText(tweets[i])
 	    d = {'date':tweets[i].date, 'text':tweets[i].text, 'id':tweets[i].id, 'username':tweets[i].username,'retweets':tweets[i].retweets, 'favorites':tweets[i].favorites, 'mentions':tweets[i].mentions,'hashtags':tweets[i].hashtags, 'geo':tweets[i].geo, 'permalink':tweets[i].permalink}
 	    print '##', i+1, '## =', d
 	manager.TweetHelper().getCSV(tweets, 'example.csv')
